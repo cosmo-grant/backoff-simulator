4e92ca7e2fd316583f3b4d3a63f321c3fb3aaa9d
Cosmo Grant <cosmodgrant@gmail.com>
2026-04-10 21:53:17 +0100

    minimal working example

---
+++ backoff_simulator.py
@@ -0,0 +1,136 @@
+# /// script
+# requires-python = ">=3.14"
+# dependencies = []
+# ///
+
+"""
+Simulates various backoff strategies for contending writes over the network.
+
+The setup:
+- many clients, starting at the same time, request over the network to write a particular database row
+- if writes contend, only one commits: the rest are discarded and the clients have to retry
+
+You want to keep low
+- the time until all writes commit
+- the total number of requests
+
+You can keep the completion time low by making clients retry rapidly.
+But then writes often contend, so the request count is high.
+You can keep the request count low by making clients spread out their tries.
+But then the completion time is high.
+So there’s a tradeoff.
+
+There's a famous aws blog post and simulation script about this:
+- https://aws.amazon.com/blogs/architecture/exponential-backoff-and-jitter
+- https://github.com/aws-samples/aws-arch-backoff-simulator
+
+This script is my own exploration.
+"""
+
+import random
+
+import asyncio
+import time
+from itertools import count, repeat
+from typing import Iterator
+
+
+def tprint(start: float, *args, **kwargs):
+    elapsed = time.monotonic() - start
+    assert elapsed >= 0
+    print(f"{elapsed:.2f}:", *args, **kwargs)
+
+
+class Server:
+    """
+    Simulates a server receiving contending write requests over the network.
+
+    When it receives a request:
+        - if available, it accepts it and becomes unavailable for a while
+        - if unavailable, it rejects it
+    """
+
+    def __init__(self, duration: int):
+        self.available = True
+        self.duration = duration
+        self.first_receive_time: float | None = None
+
+    async def receive(self, id: int) -> bool:
+        if self.first_receive_time is None:
+            self.first_receive_time = time.monotonic()
+
+        if self.available:
+            tprint(self.first_receive_time, f"accepted client {id}")
+            self.available = False
+            await asyncio.sleep(self.duration)
+            self.available = True
+            return True
+        else:
+            tprint(self.first_receive_time, f"rejected client {id}")
+            return False
+
+
+class Client:
+    """
+    Simulates a client sending write requests to a server over the network.
+
+    If a request succeeds, it stops.
+    If a request fails, it backs off and retries.
+    """
+
+    ids = count()
+
+    def __init__(self, server: Server, backoffs: Iterator[float]):
+        self.id = next(self.ids)
+        self.server = server
+        self.backoffs = backoffs
+        self.request_count = 0
+
+    async def send(self):
+        accepted = False
+        while True:
+            accepted = await self.server.receive(self.id)
+            self.request_count += 1
+            if accepted:
+                break
+            await asyncio.sleep(next(self.backoffs))
+
+
+class Simulation:
+    def __init__(self, server_duration: int, backoffs: Iterator[float], num_clients: int, label: str):
+        self.server = Server(server_duration)
+        self.backoffs = backoffs
+        self.clients = [Client(self.server, backoffs) for _ in range(num_clients)]
+        self.num_clients = num_clients
+        self.label = label
+        self.start: float
+
+    async def run(self):
+        print(f"== {self.label} ==")
+        self.start = time.monotonic()
+        await asyncio.gather(*(client.send() for client in self.clients))
+        print(f"TOTAL REQUESTS: {sum(client.request_count for client in self.clients)}")
+        print(f"COMPLETION TIME: {round(time.monotonic() - self.start)}s")
+        print()
+
+
+def expo(base: int, cap: float) -> Iterator[float]:
+    return (min(cap, base * 2**n) for n in count())
+
+
+def expo_full_jitter(base: int, cap: float) -> Iterator[float]:
+    return (random.uniform(0, t) for t in expo(base, cap))
+
+
+async def main():
+    simulations = [
+        Simulation(5, repeat(3), 2, "always 3"),
+        Simulation(5, expo(2, 10), 2, "expo"),
+        Simulation(5, expo_full_jitter(2, 10), 2, "expo full jitter"),
+    ]
+    for sim in simulations:
+        await sim.run()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
